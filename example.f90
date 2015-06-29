77fe1470a4ed256f9324563210508f6b44e79df2
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-06-29 12:45:52 +0200

    add example for default value

--- example.f90
+++ example.f90
@@ -6,6 +6,8 @@ program test_PT
 
   double precision :: alpha
 
+  integer :: i
+
   open(11,file='example_file')
 
   write(11,*) 'alpha = 1.d0'
@@ -16,8 +18,28 @@ program test_PT
 
   call PTparse(CF,'example_file',5)
 
+  write(*,*) 'default value for i used'
   alpha = PTread_d(CF,'alpha')
   write(*,*) 'alpha = ', alpha
+
+  i = PTread_i(CF, 'i', -1)
+  write(*,*) 'i     = ', i
+
+  call PTkill(CF)
+
+  open(11,file='example_file')
+  write(11,*) 'alpha = 1.d0'
+  write(11,*) 'i     = 5'
+  close(11)
+
+  call PTparse(CF,'example_file',5)
+
+  write(*,*) 'default value for i not used'
+  alpha = PTread_d(CF,'alpha')
+  write(*,*) 'alpha = ', alpha
+
+  i = PTread_i(CF, 'i', -1)
+  write(*,*) 'i     = ', i
 
   call PTkill(CF)
 
