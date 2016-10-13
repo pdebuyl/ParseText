dd8b5962973cb3553ccb5e46f8c851caaf5f5abc
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-10-13 11:12:30 +0200

    update test/CMakeLists.txt and add failing test

---
+++ test/test_fail_1.f90
@@ -0,0 +1,21 @@
+program test_PT
+  use ParseText
+  use tester
+  implicit none
+
+  type(PTo) :: CF
+  type(tester_t) :: tester
+
+  double precision :: x
+
+  call PTparse(CF,'sample_file',5)
+  call tester% init()
+
+  x = PTread_d(CF,'x')
+  call tester% assert_equal(x, 2.d0)
+
+  call PTkill(CF)
+
+  call tester% print()
+
+end program test_PT
